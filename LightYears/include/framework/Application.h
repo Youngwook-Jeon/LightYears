679d295970a5ee36a69b30eec184446783ec91c6
Youngwook-Jeon <ela87@naver.com>
2024-01-01 20:49:43 +0900

    Setup the game loop

---
+++ LightYears/include/framework/Application.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+namespace ly
+{
+    class Application
+    {
+    public:
+        Application();
+        void Run();
+
+    private:
+        void Tick(float deltaTime);
+        void Render();
+
+        sf::RenderWindow mWindow;
+        float mTargetFrameRate;
+        sf::Clock mTickClock;
+    };
+}
---
+++ LightYears/src/framework/Application.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "framework/Application.h"
+
+namespace ly
+{
+    Application::Application()
+        : mWindow{ sf::VideoMode(1024, 1440), "Light Years" },
+        mTargetFrameRate{60.f},
+        mTickClock{}
+    {
+        
+    }
+
+    void Application::Run()
+    {
+        mTickClock.restart();
+        float accummulatedTime = 0.f;
+        float targetDeltaTime = 1.f / mTargetFrameRate;
+        while (mWindow.isOpen())
+        {
+            sf::Event windowEvent;
+            while (mWindow.pollEvent(windowEvent))
+            {
+                if (windowEvent.type == sf::Event::EventType::Closed)
+                {
+                    mWindow.close();
+                }
+            }
+
+            accummulatedTime += mTickClock.restart().asSeconds();
+            while (accummulatedTime > targetDeltaTime)
+            {
+                accummulatedTime -= targetDeltaTime;
+                Tick(targetDeltaTime);
+                Render();
+            }
+        }
+    }
+
+    void Application::Tick(float deltaTime)
+    {
+        std::cout << "ticking at framerate: " << 1.f / deltaTime << std::endl;
+    }
+
+    void Application::Render()
+    {
+        
+    }
+}
